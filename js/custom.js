e66d2158911eddb798bf0e87ae7d3912a5226682
Krushak81 <balintbenedikt@googlemail.com>
2016-11-10 15:57:28 +0100

    --- Fehlerkorrekturen --- Galerie Modal gefixt ---  Navigation responsive gefixt --- Profil Seite fertig

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,31 @@
-$('.thumbnail').click(function(){
-    $('.modal-body').empty();
-    var title = $(this).parent('a').attr("title");
-    $('.modal-title').html(title);
-    $($(this).parents('div').html()).appendTo('.modal-body');
-    $('#myModal').modal({show:true});
+$(document).ready(function() {
+    $('.show-image').click(function(){
+
+        $('.modal-body').empty();
+        var title = $(this).parent('a').attr("title");
+        $('.modal-title').html(title);
+        $($(this).parent('div').parent('div').html()).appendTo('.modal-body');
+        $('#myModal').modal({show:true});
+    });
 });
 
-
+$(".navbar-nav li a").on('click', function(){
+    $(".navbar-collapse").collapse('hide');
+    alert("Test");//bootstrap 3.x by Richard
+});
 
    $('li > a').click(function() {
         $('li').removeClass();
         $(this).parent().addClass('active');
     });
+
+
+
+
+$('.nav .navbar-toggle').on('click', function(){
+    $('.navbar-toggle').click();
+    alert("Test");//bootstrap 3.x by Richard
+});
+
+
+
